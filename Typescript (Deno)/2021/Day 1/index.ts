91bbf3ecae8a425bbcbb9f0ec0415dcbccab93d5
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-01 13:06:29 +0000

    TS/2021/1

---
+++ Typescript (Deno)/2021/Day 1/index.ts
@@ -0,0 +1,33 @@
+const text = Deno.readTextFileSync("input.txt");
+const input = text.split('\n').map(x => parseInt(x));
+
+function Part1(input: number[]): number {
+    let largerMeasurements = 0;
+    let previousDepth = Infinity;
+    input.forEach(depth => {
+        if (depth > previousDepth) largerMeasurements++;
+        previousDepth = depth;
+    });
+    return largerMeasurements;
+}
+
+function Part2(input: number[]) {
+    const windowSize = 3;
+    let largerWindows = 0;
+
+    let previousWindowSum = 0;
+    for (let depth = 0; depth < input.length; depth++) {
+        // Check for end
+        if (input[depth + windowSize] === undefined) return largerWindows;
+
+        let currentWindowSum = 0;
+        // Sum all depths in current window
+        for (let d = 0; d < windowSize; d++) {
+            currentWindowSum += input[depth + d];
+        }
+
+        if (currentWindowSum > previousWindowSum) largerWindows++;
+
+        previousWindowSum = currentWindowSum;
+    }
+}
