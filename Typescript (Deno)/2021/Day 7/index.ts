c16e739fbf11c78ebde86aa4170ab09b2ad85a87
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-07 21:11:00 +0000

    TS/2021/7

---
+++ Typescript (Deno)/2021/Day 7/index.ts
@@ -0,0 +1,50 @@
+const text = Deno.readTextFileSync("input.txt");
+const input = text.split(',').map((x: string) => parseInt(x));
+
+function Part1(input: number[]) {
+    const min = Math.min(...input);
+    const max = Math.max(...input);
+
+    let lowestFuel = Infinity;
+
+    for (let crab = min; crab <= max; crab++) {
+        let currentFuel = 0;
+        
+        for (let testCrab = 0; testCrab < input.length; testCrab++) {
+            if (crab === input[testCrab]) continue;
+
+            const diff = Math.abs(input[testCrab] - crab);
+
+            currentFuel += diff;
+        }
+
+        if (currentFuel < lowestFuel) lowestFuel = currentFuel;
+    }
+
+    return lowestFuel
+}
+
+function Part2(input: number[]) {
+    const min = Math.min(...input);
+    const max = Math.max(...input);
+
+    let lowestFuel = Infinity
+
+    for (let crab = min; crab <= max; crab++) {
+        let currentFuel = 0;
+
+        for (let testCrab = 0; testCrab < input.length; testCrab++) {
+            if (crab === input[testCrab]) continue;
+
+            const diff = Math.abs(input[testCrab] - crab);
+
+            // Like addition factorial I guess
+            currentFuel += (diff * (diff + 1)) / 2;
+        }
+
+        if (currentFuel < lowestFuel) lowestFuel = currentFuel;
+    }
+
+    return lowestFuel
+}
+console.log(Part2(input))
