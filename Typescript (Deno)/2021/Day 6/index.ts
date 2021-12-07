0e8ac75b528abcfe9076c92d808e9094c962ba47
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-07 15:24:00 +0000

    TS/2021/6

--- Typescript (Deno)/2021/Day 6/index.ts
+++ Typescript (Deno)/2021/Day 6/index.ts
@@ -19,3 +19,30 @@ function Part1(input: number[]) {
     
     return fish.length
 }
+
+// Naïve approach; does not work. Will take many years. Starts taking 1 minute per 'day' at approx 300,000 string length.
+function Part2(input: number[]) {
+    function setCharAt(str: string ,index: number, chr: string) {
+        if (index > str.length-1) return str;
+        return str.substring(0,index) + chr + str.substring(index+1);
+    }
+
+    const days = 256;
+    let fish: string = input.map(x => x.toString()).join('');
+
+    for (let day = 1; day < days + 1; day++) {
+        console.log(`Day: ${day}: ${fish.length}`);
+
+        const fishLengthAtStartOfDay = fish.length;
+        for (let char = 0; char < fishLengthAtStartOfDay; char++) {
+            if (parseInt(fish[char]) <= 0) {
+                fish = setCharAt(fish, char, '6');
+                fish += '8'
+            } else {
+                fish = setCharAt(fish, char, (parseInt(fish[char])-1).toString());
+            }
+        }
+    }
+    
+    return fish.length
+}
