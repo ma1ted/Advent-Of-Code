34c0275d7051569bcf8fa6e49efe0836a697b6ff
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-08 09:39:46 +0000

    TS/2021/3
    Update index

--- Typescript (Deno)/2021/Day 3/index.ts
+++ Typescript (Deno)/2021/Day 3/index.ts
@@ -1,5 +1,7 @@
-const text = Deno.readTextFileSync("input.txt");
-const input = text.split('\n');
+function Input(): string[] {
+    const text = Deno.readTextFileSync("input.txt");
+    return text.split('\n');
+}
 
 function Part1(input: string[]) {
     let gamma = "";
@@ -27,3 +29,43 @@ function Part1(input: string[]) {
 
     return gammaDec * epsilonDec;
 }
+
+// function Part2(input: string[]) {
+//     // Create copies of input as to not mutate it
+//     let o2Rating: string[] = input;
+//     let co2Rating: string[] = input;
+
+//     for (let bit = 0; bit < input[0].length; bit++) {
+//         let o2OneCount = 0;
+//         let co2OneCount = 0
+
+//         for (let line = 0; line < o2Rating.length; line++) {
+//             if (o2Rating[line][bit] === '1') o2OneCount++;
+//         }
+//         for (let line = 0; line < co2Rating.length; line++) {
+//             if (co2Rating[line][bit] === '1') co2OneCount++;
+//         }
+        
+//         // Filter bad values out of arrays
+//         // Keeps value if test passes
+
+//         // O2 wants to get rid of the type of bit that occurs the most
+//         // If equal pass 1
+//         // o2 test => if (1 occurs most) x => bit === '1' else x => bit === '0'
+//         if (o2Rating.length !== 1) o2Rating = o2Rating.filter(x => x[bit] === ((o2OneCount < o2Rating.length / 2)? '0': '1'));
+
+//         // CO2 wants to get rid of the type of bit that occurs the least
+//         // If equal pass 0
+//         // co2 test => if (0 occurs most) x => bit === '1' else x => bit === '0'
+//         if (co2Rating.length !== 1) co2Rating = co2Rating.filter(x => x[bit] === ((co2OneCount > co2Rating.length / 2)? '0': '1'));
+
+//     }
+    
+//     // Convert the results to base 10
+//     const o2Dec = parseInt(o2Rating[0], 2);
+//     const co2Dec = parseInt(co2Rating[0], 2);
+
+//     return o2Dec * co2Dec
+// }
+
+export { Input, Part1 }
