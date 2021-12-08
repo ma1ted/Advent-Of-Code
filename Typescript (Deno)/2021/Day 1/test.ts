5f90f6e43b286e05acaac2fdbaf8d30fafb0abc1
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-08 09:19:17 +0000

    TS/2021/1
    Add assertion test

---
+++ Typescript (Deno)/2021/Day 1/test.ts
@@ -0,0 +1,10 @@
+import { Input, Part1, Part2 } from "./index.ts";
+import { assert } from "https://deno.land/std@0.117.0/testing/asserts.ts";
+
+Deno.test("2021 Day 1 Part 1", () => {
+    assert(1288 === Part1(Input()))
+});
+
+Deno.test("2021 Day 1 Part 2", () => {
+    assert(1311 === Part2(Input()))
+});
