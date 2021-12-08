78be9b500a86a1f19d8b014b65514bc90dc8cc1c
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-08 10:49:36 +0000

    TS/2021/3
    Remove unused import

--- Typescript (Deno)/2021/Day 3/test.ts
+++ Typescript (Deno)/2021/Day 3/test.ts
@@ -1,4 +1,4 @@
-import { Input, Part1, Part2 } from "./index.ts";
+import { Input, Part1 } from "./index.ts";
 import { assert } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 
 Deno.test("2021 Day 3 Part 1", () => {
