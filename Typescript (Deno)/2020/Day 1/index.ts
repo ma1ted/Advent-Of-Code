9175366da0256d17881a7ef820ab3a2b11da6c43
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-08 11:45:07 +0000

    TS/2020/1

--- Typescript (Deno)/2020/Day 1/index.ts
+++ Typescript (Deno)/2020/Day 1/index.ts
@@ -1,5 +1,5 @@
 function Input(): number[] {
-    const text = Deno.readTextFileSync("./input.txt");
+    const text = Deno.readTextFileSync("Typescript (Deno)/2020/Day 1/input.txt");
     return text.split('\n').map(x => parseInt(x));
 }
 
