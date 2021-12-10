cf5a357179ec6923065c069191c1b53ee1ad85d8
Malted <59726149+ma1ted@users.noreply.github.com>
2021-12-10 13:47:14 +0000

    TS/2021/9

--- Typescript (Deno)/2021/Day 9/test.ts
+++ Typescript (Deno)/2021/Day 9/test.ts
@@ -2,5 +2,6 @@ import { Input, Part1 } from "./index.ts";
 import { assert } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 
 Deno.test("2021 Day 9 Part 1", () => {
+    console.log(Part1(Input()))
     assert(522 === Part1(Input()))
 });
